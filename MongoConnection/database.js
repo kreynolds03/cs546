919b6323dd8802faa99317058e5093439385c381
Ragini Wadhwa <raginiwadhwa@gmail.com>
2022-12-10 04:39:59 +0000

    new changes

--- MongoConnection/database.js
+++ MongoConnection/database.js
@@ -9,6 +9,7 @@ const database = module.exports = () => {
 
     try {
         mongoose.connect('mongodb+srv://RaginiWadhwa:74gzN5HTfBMDr6a2@finalproject.iw2gnyl.mongodb.net/final_project_2022?retryWrites=true&w=majority')
+        console.log("Database connected successfully!");
     } catch (error) {
 
     }
