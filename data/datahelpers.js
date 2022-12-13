bed4e215d3e389087eee631f0c32aee36b0f7538
Ragini Wadhwa <raginiwadhwa@gmail.com>
2022-12-13 05:28:41 +0000

    date validations

--- data/datahelpers.js
+++ data/datahelpers.js
@@ -13,8 +13,38 @@ function isString(val) {
 }
 
 
+function isValidDate(dateString)
+{
+    // First check for the pattern
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        throw "Please enter a proper date format of mm/dd/yyyy";
+
+    // Parse the date parts to integers
+    var parts = dateString.split("/");
+    var day = parseInt(parts[1], 10);
+    var month = parseInt(parts[0], 10);
+    var year = parseInt(parts[2], 10);
+
+    // Check the ranges of month and year
+    if(month == 0 || month > 12 || year > 2022 || year < 1000)
+        throw "Enter a proper date please!";
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    if (day > 0 && day <= monthLength[month - 1]) {
+        return dateString;
+    }
+
+    throw "The day does not match the month"
+};
 
 
 
 
-module.exports = { isAlpha };
+
+module.exports = { isAlpha, isValidDate };
