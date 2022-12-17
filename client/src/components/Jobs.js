cb9f9db5c9c4dc57261178d2cb6d0270b8aa1774
unknown <ghaxhaj@fordham.edu>
2022-12-17 13:46:46 -0500

    updated changes + sign up form

--- client/src/components/Jobs.js
+++ client/src/components/Jobs.js
@@ -1,7 +1,29 @@
-function Jobs(){
-    return(
-        <div>This Is The Jobs Page</div>
-    )
-}
+import React from 'react'
+import axios from 'axios'
 
-export default Jobs
+
+export default class Jobs extends React.Component {
+
+    state = {
+        data: []
+    }
+
+    componentDidMount(){
+        axios.get('https://localhost:3001/jobs')
+        .then(response => {console.log(response.data)})
+        .catch(error => {
+            console.log(error);
+        })
+    
+    }
+
+    render(){
+        return(
+            <div className='jobs'>
+                This is the Jobs COmponent
+                <button>Check Data</button>
+
+            </div>
+        )
+    }
+}
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,15 +1,76 @@
- import React from 'react'
+import axios from 'axios'
+import React from 'react'
  
  class SignUp extends React.Component {
 
+    state = {
+        firstname: '',
+        lastname: '',
+        username: '',
+        password: '',
+        email: '',
+        birthday: '',
+        bio: ''
 
-    
+    }
+
+
+    eventHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        const data = {
+            email: this.state.email,
+            password: this.state.password,
+            username: this.state.username,
+            birthday: this.state.birthday,
+            firstName: this.state.firstname,
+            lastName: this.state.lastname,
+            bio: this.state.bio
+        }
+        
+        axios.post('https://127.0.0.1:3001/register', data)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
 
     render(){
 
 
-    return(<div>Sign Up Page</div>)
-}
+    return(
+    <div className='signUp'>,
+        <form>
+                <label for="firstname">Enter First Name:</label>
+                <input type="text" id="firstname" name="firstname" onChange={this.eventHandler}></input>
+                <br></br>
+                <label for="lastname">Enter Last Name:</label>
+                <input type="text" id="lastname" name="lastname" onChange={this.eventHandler}></input>
+                <br></br>
+                <label for="username">Enter Username:</label>
+                <input type="text" id="username" name="username" onChange={this.eventHandler}></input>
+                <br></br>
+                <label for="password">Enter Password:</label>
+                <input type="text" id="password" name="password" onChange={this.eventHandler}></input>
+                <br></br>
+                <label for="email">Enter Email:</label>
+                <input type="text" id="email" name="email" onChange={this.eventHandler}></input>
+                <br></br>
+                <label for="birthday">Enter BirthDay:</label>
+                <input type="text" id="birthday" name="birthday" onChange={this.eventHandler}></input>
+                <br></br>
+                <label for="bio">Enter Bio:</label>
+                <input type="text" id="bio" name="bio" onChange={this.eventHandler}></input>
+                <br></br>
+
+                <button type="submit" onClick={this.handleSubmit}>Create Profile</button>
+            </form>
+    </div>
+    )}
 }
 
 export default SignUp
