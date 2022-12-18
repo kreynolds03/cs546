92294e4473653958db7b249587fe40ec808abaca
Ragini Wadhwa <raginiwadhwa@gmail.com>
2022-12-18 03:54:52 +0000

    Added the backend functionality for editing user profile

--- data/users.js
+++ data/users.js
@@ -90,7 +90,8 @@ const createUser = async (
     comments: [],
     followedUsers: [],
     likedPosts: [],
-    jobListings: []
+    jobListings: [],
+    skills: []
 
   };
 
@@ -156,9 +157,40 @@ const checkUser = async (username, password) => {
 
 };
 
+const updateUser = async (username,jobs,bio,education,skills) => {
+
+
+  let nameOfUser = username.toLowerCase();
+
+
+  const updates = 
+  {
+    jobs: jobs,
+    bio: bio,
+    education: education,
+    skills: skills,
+    username: nameOfUser
+  }
+
+  const userCollection = await users();
+
+  const updatedUserProfile = await userCollection.updateMany(
+    {username:nameOfUser},
+    {$set: updates}
+  );
+
+
+  return updates;
+
+
+
+
+}
+
 module.exports = { 
   createUser,
-  checkUser
+  checkUser,
+  updateUser
 };
 
 
