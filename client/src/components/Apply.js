8bc240624d63e8bc132680e19ffa0dcd8e25bba1
unknown <ghaxhaj@fordham.edu>
2022-12-19 16:35:01 -0500

    stopped error

--- client/src/components/Apply.js
+++ client/src/components/Apply.js
@@ -15,18 +15,20 @@ function onChangeHandler(files) {
 }
 
     return (
-        <h3>{job.jobTitle}</h3>
+        <div>
+                <h3>{job.jobTitle}</h3>
                 <h4>Company: {job.company}</h4>
                 <p>Description: {job.description}</p>
                 <p>Job ID: {job._id.toString()}</p>
                 <p>Education: {job.education}</p>
                 <p>Post Date: {job.postDate}</p> 
                 <p>Click below to upload your resume:</p>
-                <input type="file" name="file" onChange={onChangeHandler(event.target.files) => {}}/>
+                {/* <input type="file" name="file" onChange={onChangeHandler(event.target.files) => {}}/> */}
+                <input type="file" name="file" onChange={this.onChangeHandler}/>
 
+                </div>
     )
 
-    <input type="file" name="file" onChange={this.onChangeHandler}/>
 
 
 
